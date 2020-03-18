66c1e61c4f13a44809926c81463e9115e0f88d09
wuchenyv1990 <350200039@qq.com>
2020-03-18 20:16:00 +0800

    mutiple HystrixConcurrencyStrategy support

--- x-tcc-transaction-support/src/main/java/com/wcyv90/x/tcc/tx/hystrix/RequestContextConcurrencyStrategy.java
+++ x-tcc-transaction-support/src/main/java/com/wcyv90/x/tcc/tx/hystrix/RequestContextConcurrencyStrategy.java
@@ -1,18 +1,99 @@
 package com.wcyv90.x.tcc.tx.hystrix;
 
+import com.netflix.hystrix.HystrixThreadPoolKey;
+import com.netflix.hystrix.HystrixThreadPoolProperties;
+import com.netflix.hystrix.strategy.HystrixPlugins;
 import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariable;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariableLifecycle;
+import com.netflix.hystrix.strategy.eventnotifier.HystrixEventNotifier;
+import com.netflix.hystrix.strategy.executionhook.HystrixCommandExecutionHook;
+import com.netflix.hystrix.strategy.metrics.HystrixMetricsPublisher;
+import com.netflix.hystrix.strategy.properties.HystrixPropertiesStrategy;
+import com.netflix.hystrix.strategy.properties.HystrixProperty;
 import com.wcyv90.x.tcc.tx.core.TccTransaction;
 import com.wcyv90.x.tcc.tx.core.TccTransactionManager;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class RequestContextConcurrencyStrategy extends HystrixConcurrencyStrategy {
 
+    private HystrixConcurrencyStrategy delegate;
+
+    public RequestContextConcurrencyStrategy() {
+        this.delegate = HystrixPlugins.getInstance().getConcurrencyStrategy();
+        if (this.delegate instanceof RequestContextConcurrencyStrategy) {
+            return;
+        }
+        HystrixCommandExecutionHook commandExecutionHook = HystrixPlugins
+                .getInstance().getCommandExecutionHook();
+        HystrixEventNotifier eventNotifier = HystrixPlugins.getInstance()
+                .getEventNotifier();
+        HystrixMetricsPublisher metricsPublisher = HystrixPlugins.getInstance()
+                .getMetricsPublisher();
+        HystrixPropertiesStrategy propertiesStrategy = HystrixPlugins.getInstance()
+                .getPropertiesStrategy();
+        HystrixPlugins.reset();
+        HystrixPlugins.getInstance().registerConcurrencyStrategy(this);
+        HystrixPlugins.getInstance().registerCommandExecutionHook(commandExecutionHook);
+        HystrixPlugins.getInstance().registerEventNotifier(eventNotifier);
+        HystrixPlugins.getInstance().registerMetricsPublisher(metricsPublisher);
+        HystrixPlugins.getInstance().registerPropertiesStrategy(propertiesStrategy);
+
+    }
+
     @Override
     public <T> Callable<T> wrapCallable(Callable<T> callable) {
-        return super.wrapCallable(callable);
+        if (callable instanceof RequestContextTransmitter) {
+            return callable;
+        }
+        Callable<T> wrappedCallable = this.delegate != null
+                ? this.delegate.wrapCallable(callable)
+                : callable;
+        if (wrappedCallable instanceof RequestContextTransmitter) {
+            return wrappedCallable;
+        }
+        return new RequestContextTransmitter<>(callable);
+    }
+
+    @Override
+    public BlockingQueue<Runnable> getBlockingQueue(int maxQueueSize) {
+        return delegate != null
+                ? delegate.getBlockingQueue(maxQueueSize)
+                : super.getBlockingQueue(maxQueueSize);
+    }
+
+    @Override
+    public <T> HystrixRequestVariable<T> getRequestVariable(
+            HystrixRequestVariableLifecycle<T> rv) {
+        return delegate != null
+                ? delegate.getRequestVariable(rv)
+                : super.getRequestVariable(rv);
+    }
+
+    @Override
+    public ThreadPoolExecutor getThreadPool(HystrixThreadPoolKey threadPoolKey,
+                                            HystrixProperty<Integer> corePoolSize,
+                                            HystrixProperty<Integer> maximumPoolSize,
+                                            HystrixProperty<Integer> keepAliveTime, TimeUnit unit,
+                                            BlockingQueue<Runnable> workQueue) {
+        return delegate != null
+                ? delegate.getThreadPool(threadPoolKey, corePoolSize,
+                maximumPoolSize, keepAliveTime, unit, workQueue)
+                : super.getThreadPool(threadPoolKey, corePoolSize, maximumPoolSize,
+                keepAliveTime, unit, workQueue);
+    }
+
+    @Override
+    public ThreadPoolExecutor getThreadPool(HystrixThreadPoolKey threadPoolKey, HystrixThreadPoolProperties threadPoolProperties) {
+        return delegate != null
+                ? delegate.getThreadPool(threadPoolKey, threadPoolProperties)
+                : super.getThreadPool(threadPoolKey, threadPoolProperties);
     }
 
     static class RequestContextTransmitter<T> implements Callable<T> {
@@ -21,7 +102,7 @@ public class RequestContextConcurrencyStrategy extends HystrixConcurrencyStrateg
         private final RequestAttributes requestAttributes;
         private final TccTransaction tccTransaction;
 
-        public RequestContextTransmitter(Callable<T> callable) {
+        RequestContextTransmitter(Callable<T> callable) {
             this.delegate = callable;
             this.requestAttributes = RequestContextHolder.getRequestAttributes();
             this.tccTransaction = TccTransactionManager.currentTccTx();
